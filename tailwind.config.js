bcd327dd9461f5df7a10e328c7e0e7b0cf3f7a90
Alvaro Condori <con21033@byui.edu>
2023-12-23 12:50:17 -0300

    Responsive Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1565D8",
+        dark: {
+          hard: "#0D2436",
+          soft: "#183856",
+        },
+      },
+      fontFamily: {
+        opensans: ["'Open Sans'", "sans-serif"],
+        roboto: ["'Roboto'", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
